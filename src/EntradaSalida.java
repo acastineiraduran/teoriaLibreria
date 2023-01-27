1ba3bda180aedb76fd37bb5dc266634f6ab3262e
Angel Castineira Duran <acastineiraduran@danielcastelao.org>
2023-01-27 09:39:38 +0100

    modificacion

--- src/EntradaSalida.java
+++ src/EntradaSalida.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class EntradaSalida {
 
-    static final int SALIDA_CONSOLA = 1;
+    static final int SALIDA_CONSOLA = 1; // no va a variar
     static final int SALIDA_WINDOW = 2;
 
     /**
@@ -20,17 +20,18 @@ public class EntradaSalida {
                     System.out.println("Consola: " + msj);
                     return true;
                 } catch (Exception e) {
-                    return false;
+                    return false; // no se pone el break porque el return ya me saca
                 }
 
             case SALIDA_WINDOW:
-                try {
+                try { // meto todo lo que sea sensible de que me rompa
                     JOptionPane.showMessageDialog(null,"Sale por un ventana " + msj.toCharArray()[10]);
                     return true;
                 } catch(Exception e) {
                     System.out.println(e.getMessage());
                     return false;
                 }
+                // break; // me da error porque siempre me va a salir despues del return
             default:
                 return false;
         }
